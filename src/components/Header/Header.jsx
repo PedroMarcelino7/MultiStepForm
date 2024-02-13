2794f3eb90b77207d10b1418810bf0b3a72974a0
Pedro Marcelino <pedropmarcelino2004@gmail.com>
2024-02-13 20:00:25 -0300

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import styles from './Header.module.css'
+
+export default function Header({ title, subtitle }) {
+    return (
+        <>
+            <h1>{title}</h1>
+            <h2>{subtitle}</h2>
+        </>
+    )
+}
